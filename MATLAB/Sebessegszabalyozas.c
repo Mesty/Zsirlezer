db12095f062bdc6427078557dc29183b81293a5c
Mester Adam <adam.mester@invitel.hu>
2017-02-08 20:24:20 +0100

    Sebessegszabalyozas kodja kijavitva. Idoallando 0.5s, beavatkozo szerv telites hatasa 1000-es Compare Value-nal

--- MATLAB/Sebessegszabalyozas.c
+++ MATLAB/Sebessegszabalyozas.c
@@ -1,10 +1,4 @@
-// Alapjel meredekseg korlatja [m/s^2]
-#define ALAPJEL_MEREDEKSEG_GYORSITASNAL 3000
-#define ALAPJEL_MEREDEKSEG_LASSITASNAL 3000
-// Beavatkozo jel meredekseg korlatja [100/s]
-// #define BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL 2000
-// #define BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL 2000
-
+// Belso valtozok
 int32_t alapjel = mmpersec;
 static int32_t elozo_alapjel = 0;
 static float beavatkozo_jel = 0;
@@ -13,22 +7,17 @@ static float pozitiv_visszacsatolas = 0;
 float FOXBORO_bemeno_jel = 0;
  
 // Szabalyozas
-if(alapjel - elozo_alapjel > ALAPJEL_MEREDEKSEG_GYORSITASNAL)
-	alapjel = elozo_alapjel + ALAPJEL_MEREDEKSEG_GYORSITASNAL;
-else if(elozo_alapjel - alapjel > ALAPJEL_MEREDEKSEG_LASSITASNAL)
-	alapjel = elozo_alapjel - ALAPJEL_MEREDEKSEG_LASSITASNAL;
-pozitiv_visszacsatolas = 0.99288*pozitiv_visszacsatolas+0.0071168*elozo_beavatkozo_jel;
-FOXBORO_bemeno_jel = 0.72413*(alapjel-velocity)+pozitiv_visszacsatolas;
-if(FOXBORO_bemeno_jel > 2891.7847)
-	beavatkozo_jel = 2891.7847;
-else if(FOXBORO_bemeno_jel < -2891.7847)
-	beavatkozo_jel = -2891.7847;
+// Szabalyozasi algoritmus
+pozitiv_visszacsatolas = 0.99288*pozitiv_visszacsatolas+0.0071168*beavatkozo_jel;
+FOXBORO_bemeno_jel = 0.72413*(mmpersec-velocity)+pozitiv_visszacsatolas;
+// Beavatkozo szerv telites kezelese
+if(FOXBORO_bemeno_jel > 1000)
+	beavatkozo_jel = 1000;
+else if(FOXBORO_bemeno_jel < -1000)
+	beavatkozo_jel = -1000;
 else
 	beavatkozo_jel = FOXBORO_bemeno_jel;
-// if(beavatkozo_jel - elozo_beavatkozo_jel > BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL)
-	// beavatkozo_jel = elozo_beavatkozo_jel + BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL;
-// else if(elozo_beavatkozo_jel - beavatkozo_jel > BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL)
-	// beavatkozo_jel = elozo_beavatkozo_jel - BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL;
+// Inverz statikus nemlinearitas
 if(beavatkozo_jel > 0)
 {
 	if((0 < beavatkozo_jel) && (beavatkozo_jel <= 112.0907))
@@ -42,6 +31,8 @@ if(beavatkozo_jel > 0)
 	else if((756.6123 < beavatkozo_jel) && (beavatkozo_jel <= 910.737))
 		beavatkozo_jel = 600+0.64883*(beavatkozo_jel-756.6123);
 }
+// Beavatkozo jel kiadasa
 motorpulsePWM = (uint32_t) (beavatkozo_jel+6932);
+// Integratorok feltoltese
 elozo_alapjel = alapjel;
 elozo_beavatkozo_jel = beavatkozo_jel;
