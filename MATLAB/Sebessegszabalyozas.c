3ae54912b5ea227359fad2f41a45b77c98d29f03
Mester Adam <adam.mester@invitel.hu>
2017-02-08 18:42:34 +0100

    Sebessegszabalyozas MATLAB-bol generalt C kodja javitva

--- MATLAB/Sebessegszabalyozas.c
+++ MATLAB/Sebessegszabalyozas.c
@@ -1,23 +1,24 @@
-// Alapjel meredekseg korlatja
+// Alapjel meredekseg korlatja [m/s^2]
 #define ALAPJEL_MEREDEKSEG_GYORSITASNAL 3000
 #define ALAPJEL_MEREDEKSEG_LASSITASNAL 3000
 // Beavatkozo jel meredekseg korlatja [100/s]
 // #define BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL 2000
 // #define BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL 2000
 
-static float elozo_alapjel = 0;
+int32_t alapjel = mmpersec;
+static int32_t elozo_alapjel = 0;
 static float beavatkozo_jel = 0;
 static float elozo_beavatkozo_jel = 0;
 static float pozitiv_visszacsatolas = 0;
 float FOXBORO_bemeno_jel = 0;
  
 // Szabalyozas
-if(mmpersec - elozo_beavatkozo_jel > ALAPJEL_MEREDEKSEG_GYORSITASNAL)
-	beavatkozo_jel = elozo_beavatkozo_jel + ALAPJEL_MEREDEKSEG_GYORSITASNAL;
-else if(elozo_beavatkozo_jel - beavatkozo_jel > ALAPJEL_MEREDEKSEG_LASSITASNAL)
-	beavatkozo_jel = elozo_beavatkozo_jel - ALAPJEL_MEREDEKSEG_LASSITASNAL;
+if(alapjel - elozo_alapjel > ALAPJEL_MEREDEKSEG_GYORSITASNAL)
+	alapjel = elozo_alapjel + ALAPJEL_MEREDEKSEG_GYORSITASNAL;
+else if(elozo_alapjel - alapjel > ALAPJEL_MEREDEKSEG_LASSITASNAL)
+	alapjel = elozo_alapjel - ALAPJEL_MEREDEKSEG_LASSITASNAL;
 pozitiv_visszacsatolas = 0.99288*pozitiv_visszacsatolas+0.0071168*elozo_beavatkozo_jel;
-FOXBORO_bemeno_jel = 0.72413*(mmpersec-velocity)+pozitiv_visszacsatolas;
+FOXBORO_bemeno_jel = 0.72413*(alapjel-velocity)+pozitiv_visszacsatolas;
 if(FOXBORO_bemeno_jel > 2891.7847)
 	beavatkozo_jel = 2891.7847;
 else if(FOXBORO_bemeno_jel < -2891.7847)
@@ -42,5 +43,5 @@ if(beavatkozo_jel > 0)
 		beavatkozo_jel = 600+0.64883*(beavatkozo_jel-756.6123);
 }
 motorpulsePWM = (uint32_t) (beavatkozo_jel+6932);
-elozo_alapjel = mmpersec;
+elozo_alapjel = alapjel;
 elozo_beavatkozo_jel = beavatkozo_jel;
