356fa01b56e9a6c50fca11107417bdb9634dd868
Mester Adam <adam.mester@invitel.hu>
2017-02-08 15:54:08 +0100

    Sebessegszabalyozas MATLAB-bol generalt C kodjabol kikommentezve a beavatkozo jel meredekseg korlatja, alapjel meredekseg korlatja felveve

--- MATLAB/Sebessegszabalyozas.c
+++ MATLAB/Sebessegszabalyozas.c
@@ -1,13 +1,21 @@
+// Alapjel meredekseg korlatja
+#define ALAPJEL_MEREDEKSEG_GYORSITASNAL 3000
+#define ALAPJEL_MEREDEKSEG_LASSITASNAL 3000
 // Beavatkozo jel meredekseg korlatja [100/s]
-#define MEREDEKSEG_GYORSITASNAL 5
-#define MEREDEKSEG_LASSITASNAL 5
+// #define BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL 2000
+// #define BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL 2000
 
+static float elozo_alapjel = 0;
 static float beavatkozo_jel = 0;
-static float elozo_beavatkozo_jel = 0;
+// static float elozo_beavatkozo_jel = 0;
 static float pozitiv_visszacsatolas = 0;
 float FOXBORO_bemeno_jel = 0;
  
 // Szabalyozas
+if(mmpersec - elozo_beavatkozo_jel > ALAPJEL_MEREDEKSEG_GYORSITASNAL)
+	beavatkozo_jel = elozo_beavatkozo_jel + ALAPJEL_MEREDEKSEG_GYORSITASNAL;
+else if(elozo_beavatkozo_jel - beavatkozo_jel > ALAPJEL_MEREDEKSEG_LASSITASNAL)
+	beavatkozo_jel = elozo_beavatkozo_jel - ALAPJEL_MEREDEKSEG_LASSITASNAL;
 pozitiv_visszacsatolas = 0.99288*pozitiv_visszacsatolas+0.0071168*elozo_beavatkozo_jel;
 FOXBORO_bemeno_jel = 0.72413*(mmpersec-velocity)+pozitiv_visszacsatolas;
 if(FOXBORO_bemeno_jel > 2891.7847)
@@ -16,10 +24,10 @@ else if(FOXBORO_bemeno_jel < -2891.7847)
 	beavatkozo_jel = -2891.7847;
 else
 	beavatkozo_jel = FOXBORO_bemeno_jel;
-if(beavatkozo_jel - elozo_beavatkozo_jel > MEREDEKSEG_GYORSITASNAL)
-	beavatkozo_jel = elozo_beavatkozo_jel + MEREDEKSEG_GYORSITASNAL;
-else if(elozo_beavatkozo_jel - beavatkozo_jel > MEREDEKSEG_LASSITASNAL)
-	beavatkozo_jel = elozo_beavatkozo_jel - MEREDEKSEG_LASSITASNAL;
+// if(beavatkozo_jel - elozo_beavatkozo_jel > BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL)
+	// beavatkozo_jel = elozo_beavatkozo_jel + BEAVATKOZO_JEL_MEREDEKSEG_GYORSITASNAL;
+// else if(elozo_beavatkozo_jel - beavatkozo_jel > BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL)
+	// beavatkozo_jel = elozo_beavatkozo_jel - BEAVATKOZO_JEL_MEREDEKSEG_LASSITASNAL;
 if(beavatkozo_jel > 0)
 {
 	if((0 < beavatkozo_jel) && (beavatkozo_jel <= 112.0907))
@@ -34,4 +42,5 @@ if(beavatkozo_jel > 0)
 		beavatkozo_jel = 600+0.64883*(beavatkozo_jel-756.6123);
 }
 motorpulsePWM = (uint32_t) (beavatkozo_jel+6932);
+elozo_alapjel = mmpersec;
 elozo_beavatkozo_jel = beavatkozo_jel;
